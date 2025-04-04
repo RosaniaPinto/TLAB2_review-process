c71fb82aa0336696c664bc0173bc22d7b44c45a2
Rosania Pinto <rosaniapinto@gmail.com>
2025-04-04 17:00:08 +0000

    label work in progress

--- label.py
+++ label.py
@@ -2,12 +2,22 @@ from openai import OpenAI
 
 
 def get_sentiment(text: list) -> list:
+    client = OpenAI()
+    completion = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {"role":"developer", "content": "You are a children's short-story writer"},
+        {"role":"developer", "content": "Write a one-sentence bedtime story about a unicorn"}
+    ]
+    )
+
     """
     INSERT DOCSTRING HERE
     """
     system_prompt = """
+    As a artist manager, if I wanted to be a musician but I have zeo fans, what are steps to get to stardom?
     ...
-    """
+    """  
 
     prompt = f"""
     For each line of text in the string below, please categorize the review
@@ -16,4 +26,4 @@ def get_sentiment(text: list) -> list:
     Use only a one-word response per line. Do not include any numbers.
     {text}
     """
-    pass
+    print(completions,choices[0].message.content)
